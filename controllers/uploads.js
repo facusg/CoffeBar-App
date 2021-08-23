e427ac77ce4879b73a4321681112041505d07274
Facundo <facusgalante@gmail.com>
2021-08-23 13:59:31 -0300

    Borrar imagenes de cloudinary que no se usan

--- controllers/uploads.js
+++ controllers/uploads.js
@@ -152,8 +152,14 @@ const actualizarImagenCloudinary = async (req, res = response) => {
   }
 
   if (modelo.img) {
+    //Borrar imagen de cloudinary
+    const nombreArr = modelo.img.split("/");
+    const nombre = nombreArr[nombreArr.length - 1];
+    const [public_id, extension] = nombre.split(".");
+    cloudinary.uploader.destroy(public_id);
   }
 
+  //Subir nueva imagen a cloudinary
   const { secure_url } = await cloudinary.uploader.upload(
     req.files.archivo.tempFilePath
   );
